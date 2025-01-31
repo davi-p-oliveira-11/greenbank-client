efb135c23b5b9c5e3fe37e11eb438b2f8e606193
davi-p-oliveira-11 <dpo.dev11@gmail.com>
2025-01-31 18:40:34 -0300

    added props to card and footer component

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,13 @@ function App() {
     <>
       <div>
         Greenbank
-        <Card />
-        <Card />
-        <Card />
+        <Card
+          id={1}
+          paragraph="Typescript"
+          details="TS para frontend e backend"
+        />
+        <Card id={2} paragraph="HTML" details="HTML para frontend" />
+        <Card id={3} paragraph="SQL" details="SQL para banco de dados" />
       </div>
       <div>Footer</div>
     </>
